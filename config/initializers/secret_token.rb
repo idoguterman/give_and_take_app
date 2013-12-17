d9b1ed7b38ce1acc3afeb80d991037a8282dcb19
Ido Guterman <idoguterman@gmail.com>
2013-12-17 16:20:30 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiveAndTakeApp::Application.config.secret_key_base = '2ab4aa543d54bd5e2df35e38205706e4cfb3e4d314a8d5e89237fee071c8ecdaa84324ea120c60c5661a6be6c5ff30f3f53544620c23cc341f50de74a37dee81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GiveAndTakeApp::Application.config.session_store :cookie_store, key: '_give_and_take_app_session'
